0a4a593709bd9f976ea1a8070ee24e377cff7c23
Guilherme Farrel de Souza <garcia.farrel@gmail.com>
2024-12-15 15:59:49 -0300

    `refactor(app): remove unused module imports`
    Cleaned up unused module imports in the AppModule to streamline dependencies. This reduces unnecessary overhead and improves maintainability of the codebase.

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -4,9 +4,6 @@ import { ConfigModule } from '@nestjs/config'
 import { AuthModule } from './auth/auth.module'
 import { envSchema } from './env/env'
 import { EnvModule } from './env/env.module'
-import { EventsModule } from './events/events.module'
-import { RolesModule } from './guards/roles.module'
-import { HttpModule } from './http/http.module'
 
 @Module({
   imports: [
@@ -15,10 +12,7 @@ import { HttpModule } from './http/http.module'
       isGlobal: true,
     }),
     AuthModule,
-    RolesModule,
-    HttpModule,
     EnvModule,
-    EventsModule,
   ],
 })
 export class AppModule {}
